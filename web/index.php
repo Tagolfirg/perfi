d86478fff65de5306dda61b88c3b7d91f553c9c4
Timur Melnikov <melnikovt@gmail.com>
2017-08-10 10:33:33 +0300

    Глобальная конфигурация виджета LoadingOverlayPjax

--- web/index.php
+++ web/index.php
@@ -9,4 +9,10 @@ require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/../config/web.php');
 
+//Глобальная конфигурация виджетов
+\Yii::$container->set('timurmelnikov\widgets\LoadingOverlayPjax', [
+                        'zIndex'=>10,
+    ]
+);
+
 (new yii\web\Application($config))->run();
